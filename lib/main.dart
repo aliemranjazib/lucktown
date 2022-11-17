819febd0de9503a0eb96624bcd64a859e84413cc
Malik Abdul Ahad <malikabdulahad01@outlook.com>
2022-11-18 00:07:18 +0500

    update Today

--- lib/main.dart
+++ lib/main.dart
@@ -71,13 +71,10 @@ class MyApp extends StatelessWidget {
               return WebSetNewPinPage();
             case web_scaffold_page:
               return WebScaffold();
-<<<<<<< HEAD
             case web_topup_usdt_page:
               return TopUpMethodScreen();
-=======
             case web_profile_page:
               return ProfilePage();
->>>>>>> 20116f9e039224bdc6fd26a07a2fec3922a76d2f
             default:
               return const SizedBox.shrink();
           }
@@ -102,17 +99,13 @@ class MyApp extends StatelessWidget {
         ],
       ),
       // home: web_scaffold_page,
-<<<<<<< HEAD
       // initialRoute: LuckySharedPef.getAuthToken().isEmpty
       //     ? web_scaffold_page
       //     : web_home_Page,
       initialRoute: web_topup_usdt_page,
-=======
-      initialRoute: 
-      LuckySharedPef.getAuthToken().isEmpty
-          ? web_scaffold_page
-          : web_home_Page,
->>>>>>> 20116f9e039224bdc6fd26a07a2fec3922a76d2f
+      // initialRoute: LuckySharedPef.getAuthToken().isEmpty
+      //     ? web_scaffold_page
+      //     : web_home_Page,
       // home: ResponsiveLayout(
       //     mobileScaffold: MobileScaffold(),
       //     tabletScaffold: TabletScaffold(),
