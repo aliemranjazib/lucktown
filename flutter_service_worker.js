5126835cfef671643da9e9f44e273c5101f30f33
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2022-12-07 22:18:04 +0500

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -339,9 +339,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5671b25f7ec95adf422310100de6c6c9",
-"/": "5671b25f7ec95adf422310100de6c6c9",
-"main.dart.js": "73d00054b3152fe98ca328fb7545c4b6",
+"index.html": "2998a3fad8c51894470bfbd6aefe4e16",
+"/": "2998a3fad8c51894470bfbd6aefe4e16",
+"main.dart.js": "ad8002e5c5047d266fdbd70f119ecf6c",
 "manifest.json": "43c2c4f002e72595606a862fcf94652b",
 "version.json": "029bbf2d2f2beed5e7e1421aa4587bcd"
 };
